54b8def42f134918b79d4a7d90905bc128134638
ahmedanwar1234 <ahmedanwar123@gmail.com>
2026-03-18 22:35:20 +0200

    new project

--- src/app/page.js
+++ src/app/page.js
@@ -13,19 +13,32 @@ export default function Home() {
 
     <div className="relative min-h-screen ">
       {/* Background Video for Header and Hero Section */}
-     <div className="absolute inset-0 z-0">
+  <div className="absolute inset-0 z-0">
+  {/* فيديو للشاشات الكبيرة */}
   <video
     autoPlay
     loop
     muted
     playsInline
-    className="w-full h-full object-cover" 
+    className="w-full h-full object-cover hidden md:block"
   >
     <source src="/herovideo2.mp4" type="video/mp4" />
   </video>
+
+  {/* فيديو للشاشات الصغيرة (موبايل) */}
+  <video
+    autoPlay
+    loop
+    muted
+    playsInline
+    className="w-full h-full object-cover md:hidden"
+  >
+    <source src="/herovideomobile.mp4" type="video/mp4" />
+  </video>
+
+  {/* Overlay */}
   <div className="absolute inset-0 bg-black opacity-40"></div>
 </div>
-      
       {/* Content Sections */}
       <div className="relative z-10">
         <Header/>
